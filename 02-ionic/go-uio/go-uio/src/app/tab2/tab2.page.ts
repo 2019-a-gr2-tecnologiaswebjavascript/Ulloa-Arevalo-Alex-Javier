aac2cb865253293b9d3405f7bd35605a6d8f7df9
Alex Ulloa <ajua_1696@hotmail.com>
2019-05-17 00:20:38 -0500

    Deber Ionic

--- 02-ionic/go-uio/go-uio/src/app/tab2/tab2.page.ts
+++ 02-ionic/go-uio/go-uio/src/app/tab2/tab2.page.ts
@@ -5,4 +5,43 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  fecha = new Date();  // 2009-11-10
+  month = this.fecha.toLocaleString('en-us', { month: 'long' });
+
+  listaPartidos = [
+    {
+      mes: 'May',
+      dia: '12th',
+      golesLocal: 3,
+      golesVisita: 1,
+      nombreLocal: 'Macara',
+      nombreVisita: 'Fuerza Amarilla'
+    },
+    {
+      mes: 'May',
+      dia: '12th',
+      golesLocal: 3,
+      golesVisita: 1,
+      nombreLocal: 'Macara',
+      nombreVisita: 'Fuerza Amarilla'
+    },
+    {
+      mes: 'May',
+      dia: '13th',
+      golesLocal: 0,
+      golesVisita: 0,
+      nombreLocal: 'Aucas',
+      nombreVisita: 'Universidad Catolica'
+    },
+    {
+      mes: 'May',
+      dia: '14th',
+      golesLocal: 3,
+      golesVisita: 1,
+      nombreLocal: 'Liga',
+      nombreVisita: 'Independiente del valle'
+    },
+  ];
+
+}
--- 02-ionic/go-uio/go-uio/src/app/tab3/tab3.page.ts
+++ 02-ionic/go-uio/go-uio/src/app/tab3/tab3.page.ts
@@ -5,4 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  listaBusquedas = [
+    {
+      imagen: 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Flag_of_Ecuador.svg',
+      nombre: 'Ecuador'
+    },
+    {
+      imagen: 'https://upload.wikimedia.org/wikipedia/commons/5/57/Macara_6.png',
+      nombre: 'Macara'
+    },
+    {
+      imagen: 'http://cdn.mashup-web.com/img/categories/copa-libertadores.png',
+      nombre: 'Copa libertadores'
+    },
+  ];
+}
